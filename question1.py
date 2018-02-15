a168ca44cd5273d7620831dec359b799d6db20ea
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-02-14 20:48:48 -0500

    Add check_if_solvable method

--- question1.py
+++ question1.py
@@ -18,11 +18,29 @@ def check_goal_state(puzzle):
     goal = [[1,2,3],[8,0,4],[7,6,5]]
     return goal == puzzle
 
+def check_if_solvable(puzzle):
+    puzzle_list = convert_puzzle_to_list(puzzle)
+    inversions = 0
+    for i in range(len(puzzle_list)):
+        for j in range(i+1, len(puzzle_list), 1):
+            a = puzzle_list[i]
+            b = puzzle_list[j]
+            if a != 0 and b != 0 and a > b:
+                inversions += 1
+    return inversions%2 == 1
+
+def convert_puzzle_to_list(puzzle):
+    ret_list = []
+    for row in range(3):
+        for col in range(3):
+            ret_list.append(puzzle[row][col])
+    return ret_list
+
 puzzle = generate_random()
 correct = [[1,2,3],[8,0,4],[7,6,5]]
-print(puzzle)
-print(check_goal_state(puzzle))
+# print(puzzle)
+# print(check_if_solvable(puzzle))
 print(correct)
-print(check_goal_state(correct))
+print(check_if_solvable(correct))
 
 
