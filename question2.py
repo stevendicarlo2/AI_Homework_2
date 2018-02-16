a890146fb9b52a194dad386c7c06b8929d8c8c3d
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-02-16 16:02:29 -0500

    Create propagate and contradictory methods

--- question2.py
+++ question2.py
@@ -31,6 +31,59 @@ def make_original_tree(list_of_statements):
         temp_node.left = new_node
         temp_node = new_node
     print(root_node.to_string(0, ""))
-    
 
-make_original_tree(["A B C D", "E F G H", "I J", "Kl"])
+def and_rule(start_node):
+    if "AND" not in start_node.statement:
+        return
+    index = start_node.statement.index("AND")
+    first_part = start_node.statement[:index]
+    second_part = start_node.statement[index+1:]
+    new_node1 = Node()
+    new_node1.statement = first_part
+    new_node2 = Node()
+    new_node2.statement = second_part
+    propagate_in_order(start_node, new_node1, new_node2)
+
+def propagate_in_order(start_node, node1, node2):
+    if start_node is None:
+        return
+    if start_node.left is None:
+        node1.parent = start_node
+        start_node.left = node1
+        node2.parent = node1
+        node1.left = node2
+        return
+    else:
+        propagate_in_order(start_node.left, node1, node2)
+        propagate_in_order(start_node.right, node1, node2)
+
+def propagate_split(start_node, node1, node2):
+    if start_node is None:
+        return
+    if start_node.left is None:
+        node1.parent = start_node
+        start_node.left = node1
+        node2.parent = start_node
+        start_node.right = node2
+        return
+    else:
+        propagate_in_order(start_node.left, node1, node2)
+        propagate_in_order(start_node.right, node1, node2)
+
+def check_if_contradictory(statement1, statement2):
+    if not ((len(statement1) == 1 and len(statement2) == 2) or (len(statement1) == 2 and len(statement2) == 1)):
+        return False
+    if len(statement1) == 1:
+        ret_val = True
+        ret_val = ret_val and statement2[0] == "NOT"
+        ret_val = ret_val and statement1[0] == statement2[1]
+        ret_val = ret_val and statement1[0][0].isupper() and statement1[0][1].islower()
+        return ret_val
+    else:
+        ret_val = True
+        ret_val = ret_val and statement1[0] == "NOT"
+        ret_val = ret_val and statement2[0] == statement1[1]
+        ret_val = ret_val and statement2[0][0].isupper() and statement2[0][1].islower()
+        return ret_val
+
+print(check_if_contradictory(["NOT", "Pd"], ["Pd"]))
